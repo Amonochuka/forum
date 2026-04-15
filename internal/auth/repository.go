aa5964921a6ee90089d9bb470f3211369ce458ea
Ashomondi <adhiamboa745@gmail.com>
2026-04-15 16:29:12 +0300

    added a function that gets user by email

--- internal/auth/repository.go
+++ internal/auth/repository.go
@@ -11,3 +11,24 @@ type Repository struct {
 func NewRepository(db *sql.DB) *Repository {
 	return &Repository{DB: db}
 }
+
+func (r *Repository) CreateUser(user User) error {
+	_, err := r.DB.Exec(
+		"INSERT INTO users(username, email, password) VALUES (?, ?, ?)",
+		user.Username,
+		user.Email,
+		user.Password,
+	)
+	return err
+}
+
+func (r *Repository) GetUserByEmail(email string) (User, error) {
+	var user User
+
+	err := r.DB.QueryRow(
+		"SELECT id, username, email, password, created_at FROM users WHERE email = ?",
+		email,
+	).Scan(&user.ID, &user.Username, &user.Email, &user.Password, &user.CreatedAt)
+
+	return user, err
+}
