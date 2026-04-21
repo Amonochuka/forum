84ec4a020260deb561b2d6aca78bf9bb3c9ac3e7
Ashomondi <adhiamboa745@gmail.com>
2026-04-21 10:59:17 +0300

    added a function to allow login to decode the user request

--- internal/auth/handler.go
+++ internal/auth/handler.go
@@ -15,10 +15,15 @@ func NewHandler(service *Service) *Handler {
 
 func (h *Handler) Register(w http.ResponseWriter, r *http.Request) {
 	var user User
-	json.NewDecoder(r.Body).Decode(&user)
-	err := h.Service.Register(user)
+	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
-		http.Error(w, "Cannot register user", http.StatusBadRequest)
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	err = h.Service.Register(user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 	w.WriteHeader(http.StatusCreated)
@@ -27,6 +32,12 @@ func (h *Handler) Register(w http.ResponseWriter, r *http.Request) {
 
 func (h *Handler) Login(w http.ResponseWriter, r *http.Request) {
 	var user User
+
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, "Inavlid request body", http.StatusUnauthorized)
+		return
+	}
 
 	loggedUser, err := h.Service.Login(user.Email, user.Password)
 	if err != nil {
